307af1be3dd2aa458aae70e4172825858aae6fc2
Kia Farhang <kfarhang0@gmail.com>
2018-02-24 13:10:55 -0600

    Add code + tests for a function that pings the Yelp API for a list of restaurants matching search, location criteria

--- src/yelp/index.ts
+++ src/yelp/index.ts
@@ -1,7 +1,26 @@
-import {Location } from "../types";
+import * as rp from "request-promise-native";
+import {Location, YelpSearchResponse } from "../types";
 
-export const buildSearchQueryString = (searchTerm: string, location?: Location, address?: string): string=>{
+export const buildSearchQueryString = (searchTerm: string, location?: Location, address?: string): string => {
 
-    return `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&${location ? `latitude=${location.latitude}&longitude=${location.longitude}` : `location=${encodeURIComponent(address as string)}`}`;
+    return `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&${location ?
+    `latitude=${location.latitude}&longitude=${location.longitude}` :
+    `location=${encodeURIComponent(address as string)}`}`;
+};
 
-}
+export const searchRestaurants = async (searchTerm: string, location: Location,
+                                        address?: string): Promise<YelpSearchResponse> => {
+                                            const parameters = {
+        uri: buildSearchQueryString(searchTerm, location, address),
+        headers: {
+        Authorization: `Bearer ${process.env.YELP_KEY}`,
+        },
+        json: true,
+    };
+                                            try {
+        const response = await rp(parameters);
+        return response;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
--- src/yelp/spec.ts
+++ src/yelp/spec.ts
@@ -1,43 +1,129 @@
 import { assert } from "chai";
+import * as dotenv from "dotenv";
+import * as nock from "nock";
+import * as sinon from "sinon";
+import {YelpBusiness, YelpSearchResponse} from "../types";
 import * as y from "./index";
+import mockData from "./yelp-api-mock";
 
-describe("Yelp", ()=>{
-    describe("buildSearchQueryString", ()=>{
+dotenv.config();
+
+describe("Yelp", () => {
+    after(() => {
+        nock.cleanAll();
+    });
+    describe("buildSearchQueryString", () => {
         const searchTerm = "hamburgers";
         const location = {
             latitude: 29.4786,
-            longitude: -98.4872
+            longitude: -98.4872,
         };
-        it("returns a string", ()=>{
+        it("returns a string", () => {
             assert.isString(y.buildSearchQueryString(searchTerm, location));
         });
-        it("the string starts with the Yelp API search endpoint", ()=>{
+        it("the string starts with the Yelp API search endpoint", () => {
             const searchString = y.buildSearchQueryString(searchTerm, location);
             const endpoint = "https://api.yelp.com/v3/businesses/search";
             const shouldBeEndpoint = searchString.substr(0, endpoint.length);
             assert.strictEqual(endpoint, shouldBeEndpoint);
         });
-        it("the string includes the search term passed in as the first query parameter", ()=>{
+        it("the string includes the search term passed in as the first query parameter", () => {
             const searchString = y.buildSearchQueryString(searchTerm, location);
             assert.include(searchString, `?term=${searchTerm}`);
         });
-        it("if passed a location, the string includes latitude and longitude query parameters", ()=>{
+        it("if passed a location, the string includes latitude and longitude query parameters", () => {
             const searchString = y.buildSearchQueryString(searchTerm, location);
             assert.include(searchString, `&latitude=${location.latitude}`);
             assert.include(searchString, `&longitude=${location.longitude}`);
         });
-        it("if passed a location, the string does NOT include a location query parameter", ()=>{
+        it("if passed a location, the string does NOT include a location query parameter", () => {
             const searchString = y.buildSearchQueryString(searchTerm, location);
             assert.notInclude(searchString, `&location=`);
         });
-        it("if passed an address and no location, the string includes a location query parameter", ()=>{
+        it("if passed an address and no location, the string includes a location query parameter", () => {
             const searchString = y.buildSearchQueryString(searchTerm, undefined, "123 Fake Street");
             assert.include(searchString, `&location=${encodeURIComponent("123 Fake Street")}`);
         });
-        it("if passed an address and no location, the string does NOT include latitude + longitude query parameters", ()=>{
+        it("if passed an address + no location, the string does NOT include lat + long query parameters", () => {
             const searchString = y.buildSearchQueryString(searchTerm, undefined, "123 Fake Street");
             assert.notInclude(searchString, `&latitude=`);
             assert.notInclude(searchString, `&longitude=`);
+        });
+    });
+    describe("searchRestaurants", () => {
+        const fakeServer = nock("https://api.yelp.com", {
+            reqheaders: {
+                authorization: `Bearer ${process.env.YELP_KEY}`,
+                host: "api.yelp.com",
+                accept: "application/json",
+            },
+        })
+        .persist()
+        .get("/v3/businesses/search")
+        .query({term: "hamburgers", latitude: 29.4786, longitude: -98.4872})
+        .reply(200, mockData);
+
+        const requestParams = {
+            location: {
+                latitude: 29.4786,
+                longitude: -98.4872,
+            },
+            searchTerm: "hamburgers",
+        };
+
+        describe("Successfully ping the Yelp API", () => {
+            let response: YelpSearchResponse;
+            let businesses: YelpBusiness[];
+            before(async () => {
+                response = await y.searchRestaurants(requestParams.searchTerm, requestParams.location);
+                businesses = response.businesses;
+            });
+
+            it("returns an object", async () => {
+                assert.isObject(response);
+            });
+            it("the object has a 'total' number property", () => {
+                assert.property(response, "total");
+                assert.isNumber(response.total);
+            });
+            it("the object has a 'businesses' property, which is an array of objects", () => {
+                assert.property(response, "businesses");
+                assert.isArray(response.businesses);
+            });
+            describe("businesses in the array", () => {
+                it("the business has a 'rating' number property", () => {
+                    businesses.forEach((business) => {
+                        assert.property(business, "rating");
+                        assert.isNumber(business.rating);
+                    });
+                });
+                it("the business has a 'price' string property", () => {
+                    businesses.forEach((business) => {
+                        assert.property(business, "price");
+                        assert.isString(business.price);
+                    });
+                });
+            });
+        });
+        describe("Error pinging the Yelp API", () => {
+            let searchStub: sinon.SinonStub;
+            before(() => {
+                searchStub = sinon.stub(y, "searchRestaurants");
+                searchStub.throws();
+            });
+            after(() => {
+                searchStub.restore();
+            });
+            it("throws an error", async () => {
+                let err;
+                try {
+                    const response = await y.searchRestaurants(requestParams.searchTerm, requestParams.location);
+                } catch (error) {
+                    err = error;
+                }
+
+                assert.typeOf(err, "Error");
+            });
         });
     });
 });
