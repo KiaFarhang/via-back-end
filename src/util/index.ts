896f94e7f0fee0befa0bf8a2dd5be55117c588ee
Kia Farhang <kfarhang0@gmail.com>
2018-02-25 09:24:24 -0600

    Add folder for utility code

---
+++ src/util/index.ts
@@ -0,0 +1,9 @@
+import {YelpBusiness} from "../types";
+
+export const buildCostString = (stars: string): string => {
+    return "hi";
+};
+
+export const calculateTimeNeeded = ({categories, rating}: YelpBusiness): number => {
+    return 4;
+};
