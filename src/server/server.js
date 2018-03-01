4bf1a773f7afdab4a5b16865404c6580d5489274
Kia Farhang <kfarhang0@gmail.com>
2018-02-28 19:49:00 -0600

    Remove server code from previous project; tweak CORS OPTIONS settings

--- src/server/server.js
+++ src/server/server.js
@@ -12,52 +12,6 @@ const port = 8889;
 const fetchTrips = require('../yelp').fetchTrips;
 
 
-function getEventData() {
-
-    request({
-        url: "https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=San-Antonio-PHP-Meetup&status=upcoming&page=50",
-        json: true
-    }, function (error, response, body) {
-
-        if (!error && response.statusCode === 200) {
-            var data = findNextEvents(body.results);
-            // figure out a better way to do this
-            // problem is the api call is async, and so when you try to pass it in it won't work
-            // app.get('/events') should be with the other app.get's down below
-            // promises?
-            app.get('/events', (req, res) => {
-                res.render('events.ejs', { data: data });
-            });
-        } else {
-            console.log('Unable to fetch data.');
-        }
-
-    });
-
-}
-
-function getPhotos() {
-
-    request({
-        url: "https://api.meetup.com/2/photo_albums?&sign=true&photo-host=public&group_id=18644645&page=20",
-        json: true
-    }, function (error, response, body) {
-
-        if (!error && response.statusCode === 200) {
-
-            var photos = getPhotosArr(body.results);
-
-            app.get('/photos', (req, res) => {
-                res.render('photos.ejs', { photos: photos });
-            });
-        } else {
-            console.log('Unable to fetch data.');
-        }
-
-    });
-
-}
-
 // app.use(function (req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Methods", "POST,OPTIONS");
@@ -65,8 +19,15 @@ function getPhotos() {
 //     next();
 // });
 
+app.options("/*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+});
+
 // app.options('*', cors()) // include before other routes
-app.options('/', cors())
+// app.options('/', cors())
 app.post('/', cors(), async (req, res) => {
     try {
         const arr = await fetchTrips(Object.assign({}, req.body, { startTime: new Date(), endTime: new Date() }));
@@ -74,7 +35,7 @@ app.post('/', cors(), async (req, res) => {
     } catch (e) {
         console.log(e);
     }
-    
+
 });
 
 app.listen(port, () => {
